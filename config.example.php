1f8141f0c093ea0eb3fa607b54ba7b5c66915da0
idawnlight <idawn@live.com>
2021-11-15 20:16:03 +0800

    update(config): example without parameters

--- config.example.php
+++ config.example.php
@@ -14,12 +14,8 @@ return [
         // Only 'running' cdn instances will be updated
         'domains' => [
             // Only sub1.another-example.com and sub2.another-example-2.com will be updated
-            // Refer to https://cloud.tencent.com/document/api/228/12965 for parameters, which are optional
-            'sub1.another-example.com' => [
-                'httpsType' => 4, // ONLY 3 or 4
-                'forceSwitch' => 3,
-                'http2' => 'on'
-            ],
+            // Note: parameters are deprecated, not passed to api
+            'sub1.another-example.com' => [],
             'sub2.another-example-2.com' => [] // without parameters
         ]
     ]
